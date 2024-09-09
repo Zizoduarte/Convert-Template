1974481faf4f6f665d82e0bba25f91062d95189d
Zizoduarte <nexduarte@gmail.com>
2024-09-09 10:36:15 -0300

    capturando o submit do formulario

---
+++ main.js
@@ -0,0 +1,37 @@
+// Cotação de moedas do dia
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
+
+const form = document.querySelector('form');
+const amount = document.querySelector('input');
+const currency = document.querySelector('select');
+
+// Manipulando o input amount para receber somente números
+amount.addEventListener('input', () => {
+  const hasCharactersRegex = /\D+/g;
+  amount.value = amount.value.replace(hasCharactersRegex, '');
+});
+
+// Capturando o evento de submit do formulário
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  switch(currency.value){
+    case 'USD':
+      convertCurrency(amount.value, USD, 'US$');
+      break;
+    case 'EUR':
+      convertCurrency(amount.value, EUR, '€');
+      break;
+    case 'GBP':
+      convertCurrency(amount.value, GBP, '£');
+      break;
+  }
+}
+
+// Função para converter a moeda
+function convertCurrency(amount, price, symbol){
+  
+  console.log(amount, price, symbol)
+}
